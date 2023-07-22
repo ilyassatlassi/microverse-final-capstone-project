32731cf4a76b2fbf1122e94ccc4b1b74909fcf38
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-22 17:47:03 +0100

    Fix linters

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::UsersController < ApplicationController
-    protect_from_forgery with: :null_session
-  
-    def reservations
-      user = User.find(params[:user_id])
-      render json: user.reservations
-    end
+  protect_from_forgery with: :null_session
+
+  def reservations
+    user = User.find(params[:user_id])
+    render json: user.reservations
   end
-  
+end
