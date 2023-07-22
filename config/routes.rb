f40b9bff0d8620c529ce8bdcc3a0fe03bb9ae5da
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-22 17:40:36 +0100

    Update Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :users, only: [] do
       resources :doctors, only: [:index, :create, :destroy]
+      end
     end
   end
 end
