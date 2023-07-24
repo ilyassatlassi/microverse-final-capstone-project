b0a1974b88df5f6da2f4247e9eb4a6e34aa39e68
Precious Betine <preciousbetine@gmail.com>
2023-07-24 19:32:06 +0100

    Update application routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index] do
-      resources :doctors, only: [:index, :create, :destroy]
-      end
+      get 'current_user', to: 'users#current'
+      resources :doctors, only: [:index, :show, :create, :destroy]
     end
   end
 end
