494d859a0d9b668ec0abca3d376f6ae376e719ac
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-07-24 12:20:39 +0100

    Add correct api routes in the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  resources :users,only: [:index] do
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index] do
       resources :doctors, only: [:index, :create, :destroy]
+      end
+    end
   end
 end
