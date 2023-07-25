a10252cb145221a82f08f2f501d5f4ed503db727
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-07-25 13:16:30 +0430

    Modify doctor model!

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,6 @@
 class Doctor < ApplicationRecord
+  has_many :reservations, dependent: :destroy
+
+  validates :name, :specialization, :consultation_fee, :hospital, :description, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :availability, inclusion: { in: [true, false] }
 end
