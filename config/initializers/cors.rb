5391120b10de3002b33ea4c818f67a74304f22dc
Precious Betine <preciousbetine@gmail.com>
2023-07-26 14:58:41 +0100

    Fix allowed cors headers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '*', headers: :any,
-    expose: ["Authorization"],
+    expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
     methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
